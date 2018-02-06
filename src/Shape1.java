0ce0d1a2c6520e7f6740ffa6a4c50f645d5dd510
lindazen <lindazenluck@gmail.com>
2018-02-05 21:35:12 -0500

    Practice Java

---
+++ src/Shape1.java
@@ -0,0 +1,15 @@
+
+public class Shape1 {
+
+	public void circle(){
+		System.out.println("shape drawn");
+	}
+	
+	
+	public void triangle(){
+		 System.out.println("circle drawn");
+	}
+	
+	
+
+}
---
+++ src/ShapeApp1.java
@@ -0,0 +1,19 @@
+
+
+public class ShapeApp1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Shape1 shape = new Shape1();
+		shape.circle();
+
+        shape.triangle();
+	}
+
+
+
+}
